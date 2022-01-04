bf8324485cb635102f85494230ddb63901f6c148
Albert Johanson <aj.dev.gt@gmail.com>
2022-01-04 13:39:45 -0600

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,20 @@
+function main () {
+
+    const canvas = document.querySelector('#glCanvas');
+    const gl = canvas.getContext('webgl');
+
+    if(gl === null) {
+        console.log('WebGL not supported, falling back on experimental-webgl');
+        return;
+    }
+
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+
+
+
+
+window.onload = main;
